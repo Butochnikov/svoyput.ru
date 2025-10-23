5e2b82b5348f1bf7d75623f0f01f374d825ee9ab
Alexey <butochnikov@gmail.com>
2025-10-23 18:02:03 +0300

    init

---
+++ app/helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+if (! function_exists('ver')) {
+    function ver()
+    {
+        return config('svoyput.version');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!-- test -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Свой путь...</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet">
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased bg-gray-900 text-white">
+        <div class="min-h-screen flex items-center justify-center">
+            <div class="relative">
+                <video class="w-[600px] h-[600px] rounded-full object-cover" src="/vid/svoy.mp4" muted loop onmouseover="this.play()" onmouseout="this.pause()"></video>
+                <div class="w-full text-center font-['Caveat'] text-xl">
+                    Свой путь... v{{ ver() }}
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
