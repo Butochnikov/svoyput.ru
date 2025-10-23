0343257c0214d8e05a3a5f813983f5b8ec422da1
Alexey <butochnikov@gmail.com>
2025-10-23 18:16:31 +0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,11 @@
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="antialiased bg-gray-900 text-white">
-        <div class="min-h-screen flex items-center justify-center px-4">
-            <div class="relative w-full max-w-[600px]">
-                <video class="w-full aspect-square rounded-full object-cover" src="/vid/svoy.mp4" muted loop onmouseover="this.play()" onmouseout="this.pause()"></video>
-                <div class="w-full text-center font-['Caveat'] text-xl">
+    <body class="antialiased bg-gray-900 text-white overflow-hidden">
+        <div class="h-screen flex items-center justify-center px-4 py-4">
+            <div class="relative w-full max-w-[600px] max-h-full flex flex-col items-center justify-center">
+                <video class="w-full max-h-[calc(100vh-8rem)] aspect-square rounded-full object-cover" src="/vid/svoy.mp4" muted loop onmouseover="this.play()" onmouseout="this.pause()"></video>
+                <div class="w-full text-center font-['Caveat'] text-xl mt-4">
                     Свой путь... v{{ ver() }}
                 </div>
             </div>
